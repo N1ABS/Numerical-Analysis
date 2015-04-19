8d041b8af4ea7ff14275137e84f0f78900427d83
ndabsagat <ndabsagat@ENIGMA>
2015-04-19 19:33:53 +0600

    second commit

--- src/numerics/NumericalAnalysis.java
+++ src/numerics/NumericalAnalysis.java
@@ -15,7 +15,7 @@ public class NumericalAnalysis {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here something that was added
     }
     
 }
